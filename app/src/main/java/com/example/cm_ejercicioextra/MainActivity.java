2eaca4887fe118b62d7eddac93b4f4ec07f2a7b1
OrlandoTele96 <orlandoglezmusic@gmail.com>
2020-04-21 18:00:20 -0500

    Initial commit

---
+++ app/src/main/java/com/example/cm_ejercicioextra/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.cm_ejercicioextra;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvTitle,tvNumber;
+    EditText etN1,etN2,etN3,etN4;
+    Button btnSort;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvTitle=findViewById(R.id.tvTitle);
+        tvNumber=findViewById(R.id.tvNumber);
+
+        etN1=findViewById(R.id.etN1);
+        etN2=findViewById(R.id.etN2);
+        etN3=findViewById(R.id.etN3);
+        etN4=findViewById(R.id.etN4);
+
+        btnSort=findViewById(R.id.btnSort);
+
+        tvTitle.setText(getResources().getString(R.string.instruction));
+        tvNumber.setText(getResources().getString(R.string.tnumbers));
+
+        btnSort.setText(getResources().getString(R.string.bsort));
+    }
+}
